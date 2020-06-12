88f8af3420850214946941ffeebc3c5893b730a0
LStokes96 <LStokes@qa.com>
2020-06-12 11:51:44 +0100

    Assessment 2

---
+++ Assessment2/Code/python2.py
@@ -0,0 +1,288 @@
+import random
+
+	# INSTRUCTIONS
+
+	# In case it is not clear, the Question appears first, then examples, then any hints and finally the function that you need to complete appears underneath:
+
+	# <QUESTION>
+
+	# <EXAMPLES>
+
+	# <HINT>
+
+	# You are NOT allowed access to the internet for this assessment, instead you should use the DOCUMENTATION that comes bundled with your Python installation.  You should already be comfortable accessing this documentation, but to summarise:
+
+	# Access Python from you CLI
+
+	# Type help() or for example help(str)
+
+
+
+	# <QUESTION 1>    
+	
+	# Given a string, return a string where for every char in the original string, there are three chars.
+    
+    # <EXAMPLES>
+
+	# one("The") → "TTThhheee"
+	# one("AAbb") → "AAAAAAbbbbbb"
+	# one("Hi-There") → "HHHiii---TTThhheeerrreee"
+
+	# <HINT>
+	# How does a for loop iterate through a string?
+
+def one(input):
+	list1 = list(input)
+	new_list = []
+	for letters in list1:
+		new_list.append(letters*3)
+	new_string = "".join(new_list)
+	return new_string
+
+
+	# <QUESTION 2>
+
+    #  Write a function which returns the boolean True if the input is only divisible by one and itself.
+    
+    # The function should return the boolean False if not.
+
+	# <EXAMPLES>
+
+    # two(3) → True
+    # two(8) → False
+
+	# <HINT>
+	# What operator will give you the remainder?
+	# Use your CLI to access the Python documentation and get help manipulating strings - help(range).
+
+def two(input):
+	if input == 1:
+		return False
+	else:
+		for x in range(2,input):
+			if (input % x == 0):
+				return False
+		return True
+
+
+	# <QUESTION 3>
+
+    # Write a function which takes an integer input, a, and returns the sum a+aa+aaa+aaaa.
+
+	# So if 2 was the input, the function should return 2+22+222+2222 which is 2468.
+
+	# <EXAMPLES>
+
+	# three(9) → 11106
+	# three(5) → 6170
+
+	# <HINT>
+	# What happens if you multiply a string by a number?
+
+def three(a):
+	one = a
+	two = str(a)*2
+	three= str(a)*3
+	four = str(a)*4
+	return (int(a) + int(two) +int(three) + int(four))
+	
+
+	# <QUESTION 4>
+
+    # Given two Strings of equal length, 'merge' them into one String.
+
+    # Do this by 'zipping' the Strings together.
+
+    # Start with the first char of the first String.
+    # Then add the first char from the second String.
+    # Then add the second char from the first String.
+    # And so on.
+
+    # Maintain case.
+
+    # You will not encounter whitespace.
+    
+    # <EXAMPLES>
+
+	# four("String","Fridge") → "SFtrriidngge"
+	# four("Dog","Cat") → "DCoagt"
+	# four("True","Tree") → "TTrrueee" 
+	# four("return","letter") → "rleettutrenr"
+
+	# <HINT>
+	# Use your CLI to access the Python documentation and get help manipulating strings - help(list.insert).
+	# How would you seperate a string into characters?
+
+def four(input1, input2):
+	list1 = list(input1)
+	list2 = list(input2)
+	i = 0
+	while i  < len(list1):
+		n = 0
+		while n < len(list2):
+			list2.insert(n,list1[i])
+			i += 1
+			n += 2
+	return  ''.join(list2)
+
+
+
+	# <QUESTION 5>
+
+	# Write a function to randomly generate a list with 5 even numbers between 100 and 200 inclusive.
+    
+    # <EXAMPLES>
+    
+    # five() → [100,102,122,198,200]
+    # five() → [108,104,106,188,200]
+    # five() → [154,102,132,178,164]
+    
+	# <HINT>
+	# There is a module which can be used to generate random numbers, this module is called random.
+	# The random module contains a function called randint.
+
+def five():
+	list1 = []
+	n = 0
+	while n < 5:
+		x = random.randint(100,200)
+		if x % 2 != 0:
+			continue
+		list1.append(x)
+		n += 1
+	return list1
+
+
+	# <QUESTION 6>
+
+	# Given a string, return the boolean True if it ends in "py", and False if not. 
+	
+	# Ignore Case.
+
+	# For Example:
+
+	# six("ilovepy") → True
+	# six("welovepy") → True
+	# six("welovepyforreal") → False
+	# six("pyiscool") → False
+
+	# <HINT>
+	# There are no hints for this question.
+    
+def six(input):
+	if input[-2::].lower() == "py":
+		return True
+	else:	
+		return False
+
+
+	# <QUESTION 7>
+
+    # Given three ints, a b c, one of them is small, one is medium and one is large. 
+	
+	# Return the boolean True if the three values are evenly spaced, so the
+	# difference between small and medium is the same as the difference between
+	# medium and large. 
+	
+	# Do not assume the ints will come to you in a reasonable order.
+    
+    # <EXAMPLES>
+
+	# seven(2, 4, 6) → True
+	# seven(4, 6, 2) → True
+	# seven(4, 6, 3) → False
+	# seven(4, 60, 9) → False
+
+	# <HINT>
+	# There is a function for lists called sort.
+	# Use the cli to access the documentation help(list.sort)
+
+def seven(a, b, c):
+	list = [a, b, c]
+	list.sort()
+	diff_1 = list[1] - list[0]
+	diff_2 = list[2] - list[1]
+	print(diff_1)
+	print(diff_2)
+	if diff_1 == diff_2:
+		return True
+	else:
+		return False
+
+
+
+
+
+	# <QUESTION 8>
+
+    # Given a string and an integer, n, return a string that removes n letters from the 'middle' of the string.
+	
+	# The string length will be at least n, and be odd when the length of the input is odd, so there will always be a 'middle'.
+
+    # <EXAMPLES>
+
+	# eight("Hello", 3) → "Ho"
+	# eight("Chocolate", 3) → "Choate"
+	# eight("Chocolate", 1) → "Choclate"
+
+	# <HINT>
+    # Use the cli to access the documentation help(str.replace)
+
+def eight(input,  a):
+	letter = len(input) - a
+	side = int(letter/2)
+	end = len(input) - side
+	new_input = input[0:side] + input[end:]
+	return new_input
+
+
+
+	# <QUESTION 9>
+
+    # Given two string inputs, if one can be made from the other return the boolean True, if not return the boolean False.
+
+	# <EXAMPLES>
+
+    # nine("god", "dog") → True
+    # nine("tree", "tiredest") → True
+    # nine("cat", "dog") → False
+    # nine("tripping", "gin") → True
+
+	# <HINT> 
+	# There are no hints for this question.
+
+def nine(string1, string2):
+	list1 = list(string1)
+	list2 = list(string2)
+	check = all(item in list1 for item in list2)
+	check_2 = all(item in list2 for item in list1)
+	if check is True or check_2 is True:	
+		return True
+	else:
+		return False
+
+
+	# <QUESTION 10>
+
+    # Write a function which takes 2 integers greater than 0, X,Y as input and generates a 2-dimensional array. 
+	
+	# The element value in the i-th row and j-th column of the array should be i*j.
+
+	# <EXAMPLES>
+
+	# ten(3,2) → [[0,0,0],[0,1,2]]
+	# ten(2,1) → [[0,0]]
+	# ten(3,4) → [[0,0,0],[0,1,2],[0,2,4],[0,3,6]]
+
+	# <HINT>
+	# Think about nesting for loops.
+
+def ten(X,Y):
+	list1 = []
+	list2 = []
+	for numbers in range(X):
+		for lists in range(Y):
+			list2.append(numbers)
+			list1.append(list2[lists])
+	return list1
+
